749a27113d8fa9264cb1c1ac6f1220d4af3ad560
inseogi77 <inseogi77@gmail.com>
2022-02-18 15:29:47 +0900

    vtk start
    vtk project start

---
+++ vtkMFCDlgEX/pch.h
@@ -0,0 +1,37 @@
+﻿// pch.h: 미리 컴파일된 헤더 파일입니다.
+// 아래 나열된 파일은 한 번만 컴파일되었으며, 향후 빌드에 대한 빌드 성능을 향상합니다.
+// 코드 컴파일 및 여러 코드 검색 기능을 포함하여 IntelliSense 성능에도 영향을 미칩니다.
+// 그러나 여기에 나열된 파일은 빌드 간 업데이트되는 경우 모두 다시 컴파일됩니다.
+// 여기에 자주 업데이트할 파일을 추가하지 마세요. 그러면 성능이 저하됩니다.
+
+#ifndef PCH_H
+#define PCH_H
+
+// 여기에 미리 컴파일하려는 헤더 추가
+#include "framework.h"
+
+
+// vtk header
+#include <vtkAutoInit.h>
+#define vtkRenderingCore_AUTOINIT 3(vtkRenderingOpenGL2, vtkInteractionStyle, vtkRenderingFreeType)
+#define vtkRenderingContext2D_AUTOINIT 1(vtkRenderingContextOpenGL2)
+#define vtkRenderingVolume_AUTOINIT 1(vtkRenderingVolumeOpenGL)
+
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkConeSource.h>
+
+#include <vtkAbstractWidget.h>
+#include <vtkAngleWidget.h>
+#include <vtkImagePlaneWidget.h>
+#include <vtkCaptionWidget.h>
+#include <vtkOrientationMarkerWidget.h>
+#include <vtkTextWidget.h>
+
+#endif //PCH_H
---
+++ vtkMFCDlgEX/vtkMFCDlgEXDlg.h
@@ -0,0 +1,43 @@
+﻿
+// vtkMFCDlgEXDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CvtkMFCDlgEXDlg 대화 상자
+class CvtkMFCDlgEXDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CvtkMFCDlgEXDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_VTKMFCDLGEX_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+public:
+	vtkSmartPointer<vtkRenderWindow> m_vtkWindow;
+	void InitVtkWindow(void* hWnd);
+	void ResizeVtkWidnow();
+
+public:
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnBnClickedButtonCone();
+};
